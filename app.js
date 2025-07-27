bd1fc4aa7aab24c88fd10d174fc477283d748dbc
Tasnim Ayan <tasnimayan22@gmail.com>
2025-07-28 01:43:57 +0600

    invoice base path added

--- app.js
+++ app.js
@@ -1,44 +1,36 @@
+require("dotenv").config();
 
-require('dotenv').config();
-
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet')
-const xss = require('xss-clean')
-const mongoSanitize = require('express-mongo-sanitize');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const mongoose = require('mongoose')
-
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // Create Express App Instance
 const app = express();
 
-
-
-
-
 // ==========  Import Application routers  ==========
-const productRouter = require('./src/routes/productRouter')
-const userRouter = require('./src/routes/userRouter')
-const salesRouter = require('./src/routes/salesRouter')
-const invoiceRouter = require('./src/routes/invoiceRouter')
-const sellerRouter = require('./src/routes/sellerRouter')
-
+const productRouter = require("./src/routes/productRouter");
+const userRouter = require("./src/routes/userRouter");
+const salesRouter = require("./src/routes/salesRouter");
+const invoiceRouter = require("./src/routes/invoiceRouter");
+const sellerRouter = require("./src/routes/sellerRouter");
 
 //    ==========    MIDDLEWARE     ==========
 
-// ==========  Base Static File Provider Path  ========== 
-app.use(express.static(path.join(__dirname, './public')));
-
+// ==========  Base Static File Provider Path  ==========
+app.use(express.static(path.join(__dirname, "./public")));
 
 // ==========  Request Limiting For Security Purpose  ==========
 const limiter = rateLimit({
-	windowMs: 30 * 60_000,
-	max: 5000,
-	message: 'Too many requests from this IP, please try again later',
+  windowMs: 30 * 60_000,
+  max: 5000,
+  message: "Too many requests from this IP, please try again later",
 });
 
 app.use(limiter);
@@ -47,67 +39,62 @@ app.use(limiter);
 app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
 app.use(helmet());
 app.use(xss());
 
-
 // ==========  Prevent NoSQL Query Injection  ==========
 app.use(mongoSanitize());
 
-
 // ==========  CORS POLICY  ==========
 app.use((req, res, next) => {
-	res.setHeader('Content-Security-Policy', 'script-src * ');
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	next();
+  res.setHeader("Content-Security-Policy", "script-src * ");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
 });
 
 // ==========  Request Visualizing  ==========
-if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
-	console.log('Morgan enabled...');
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log("Morgan enabled...");
 }
-
 
 // ==========   Database Connection   ==========
 mongoose
-  .connect(process.env.DATABASE, {autoIndex:false})
-  .then(() => console.log('MONGODB connection successful, '+"Status:200"))
-  .catch(err => console.log(err));
+  .connect(process.env.DATABASE, { autoIndex: false })
+  .then(() => console.log("MONGODB connection successful, " + "Status:200"))
+  .catch((err) => console.log(err));
 
-mongoose.connection.on('disconnected', () => {
-	console.log("======= Database Disconnected ======");
+mongoose.connection.on("disconnected", () => {
+  console.log("======= Database Disconnected ======");
 });
 
 // ==========  ROUTE HANDLERS  ==========
 app.get("/", function (req, res) {
-	console.log("API is running")
-	res.status(200).send({status:"success", message:"API is running"});
-})
+  console.log("API is running");
+  res.status(200).send({ status: "success", message: "API is running" });
+});
 
-app.set('etag', false)  // Server won't cache data // enable in production mode
-app.use('/api/v1/products/', productRouter);
-app.use('/api/v1/users/', userRouter);
-app.use('/api/v1/sales/', salesRouter);
-app.use('/api/v1/invoice/', invoiceRouter)
-app.use('/api/v1/seller/', sellerRouter)
-
+app.use("/api/v1/products/", productRouter);
+app.use("/api/v1/users/", userRouter);
+app.use("/api/v1/sales/", salesRouter);
+app.use("/api/v1/invoice/", invoiceRouter);
+app.use("/api/v1/seller/", sellerRouter);
 
 //  ==========  Invalid Route Handler  ==========
-app.all('*', (req, res, next) => {
-	const err = new Error(`No route found at ${req.originalUrl}`);
-	if (!err.statusCode) err.statusCode = 404;
-	next(err);
+app.all("*", (req, res, next) => {
+  const err = new Error(`No route found at ${req.originalUrl}`);
+  if (!err.statusCode) err.statusCode = 404;
+  next(err);
 });
 
 //  ==========  GLOBAL ERROR MIDDLEWARE  ==========
 app.use((err, req, res, next) => {
-	console.log(err)
-	err.statusCode = err.statusCode || 500;
-	res.status(err.statusCode).send({ message: err.message });
+  console.log(err);
+  err.statusCode = err.statusCode || 500;
+  res.status(err.statusCode).send({ message: err.message });
 });
 
 module.exports = app;
--- src/controllers/invoiceController.js
+++ src/controllers/invoiceController.js
@@ -1,105 +1,108 @@
-const { createInvoiceService, paymentFailService, paymentCancelService, paymentIpnService, paymentSuccessService, invoiceListService } = require("../services/invoiceServices")
+const {
+  createInvoiceService,
+  paymentFailService,
+  paymentCancelService,
+  paymentIpnService,
+  paymentSuccessService,
+  invoiceListService,
+} = require("../services/invoiceServices");
+const BASE_FRONTEND_URL = process.env.BASE_FRONTEND_URL;
 
-exports.CreateInvoice = async (req, res) =>{
-  try{
-    let data = await createInvoiceService(req.user)
-    if(!data){
-      return res.status(404).send({message:"failed"})
+exports.CreateInvoice = async (req, res) => {
+  try {
+    let data = await createInvoiceService(req.user);
+    if (!data) {
+      return res.status(404).send({ message: "failed" });
     }
-    res.status(200).send({message:"success", data:data})
+    res.status(200).send({ message: "success", data: data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ status: "failed", message: err.message });
   }
-  catch(err){
-    console.log(err)
-    res.status(500).send({status:"failed", message:err.message})
-  }
-}
+};
 
-exports.PaymentSuccess = async (req, res) =>{
-  try{
-    let data = await paymentSuccessService(req.params.trxId)
-    if(!data){
-      return res.redirect('/orders/status?payment=fail');
+exports.PaymentSuccess = async (req, res) => {
+  try {
+    let data = await paymentSuccessService(req.params.trxId);
+    if (!data) {
+      return res.redirect(`${BASE_FRONTEND_URL}/orders/status?payment=fail`);
     }
-    res.redirect('/orders/status?payment=success');
+    res.redirect(`${BASE_FRONTEND_URL}/orders/status?payment=success`);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
   }
-  catch(err){
-    console.log(err)
-    res.status(500).send()
-  }
-}
+};
 
-exports.PaymentFail = async (req, res) =>{
-  try{
-    let data = await paymentFailService(req.params.trxId)
-    if(!data){
-      return res.redirect('/orders/status?payment=fail');
+exports.PaymentFail = async (req, res) => {
+  try {
+    let data = await paymentFailService(req.params.trxId);
+    if (!data) {
+      return res.redirect(`${BASE_FRONTEND_URL}/orders/status?payment=fail`);
     }
-  
-    res.redirect('/orders/status?payment=fail');
+
+    res.redirect(`${BASE_FRONTEND_URL}/orders/status?payment=fail`);
 
     // res.status(200).send({message:"success", status:"failed"})
-
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
   }
-  catch(err){
-    console.log(err)
-    res.status(500).send()
-  }
-}
+};
 
-exports.PaymentCancel = async (req, res) =>{
-  try{
-    let data = await paymentCancelService()
-    if(!data){
-      return res.redirect('/orders/status?payment=fail');
+exports.PaymentCancel = async (req, res) => {
+  try {
+    let data = await paymentCancelService();
+    if (!data) {
+      return res.redirect(`${BASE_FRONTEND_URL}/orders/status?payment=fail`);
     }
-    res.redirect('/orders/status?payment=canceled');
+    res.redirect(`${BASE_FRONTEND_URL}/orders/status?payment=canceled`);
     // res.status(200).send({message:"success", status:"canceled"})
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
   }
-  catch(err){
-    console.log(err)
-    res.status(500).send()
-  }
-}
+};
 
-exports.PaymentIpn = async (req, res) =>{
-  try{
-    let data = await paymentIpnService(req)
-    if(!data){
-      return res.status(404).send({message:"failed"})
+exports.PaymentIpn = async (req, res) => {
+  try {
+    let data = await paymentIpnService(req);
+    if (!data) {
+      return res.status(404).send({ message: "failed" });
     }
-    res.redirect('/user/orders');
+    res.redirect("/user/orders");
     // res.status(200).send({message:"success", status:req.body.status})
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
   }
-  catch(err){
-    console.log(err)
-    res.status(500).send()
-  }
-}
+};
 
-exports.InvoiceList = async (req, res)=>{
-  try{
-    let data = await invoiceListService(req.user._id)
-    if(!data){
-      return res.status(404).send({message:"failed"})
+exports.InvoiceList = async (req, res) => {
+  try {
+    let data = await invoiceListService(req.user._id);
+    if (!data) {
+      return res.status(404).send({ message: "failed" });
     }
-    res.status(200).send({message:"success", data:data})
+    res.status(200).send({ message: "success", data: data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
   }
-  catch(err){
-    console.log(err)
-    res.status(500).send()
-  }
-}
+};
 
-exports.InvoiceProductList = async (req, res)=>{
-  try{
-    let data = await invoiceProductListService(req.user._id, req.params.invoiceId)
-    if(!data){
-      return res.status(404).send({message:"failed"})
+exports.InvoiceProductList = async (req, res) => {
+  try {
+    let data = await invoiceProductListService(
+      req.user._id,
+      req.params.invoiceId
+    );
+    if (!data) {
+      return res.status(404).send({ message: "failed" });
     }
-    res.status(200).send({message:"success", data:data})
+    res.status(200).send({ message: "success", data: data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
   }
-  catch(err){
-    console.log(err)
-    res.status(500).send()
-  }
-}
+};
